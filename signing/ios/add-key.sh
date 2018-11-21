dc0fa2ad1f5dfb918026ec3fa97c73d36d463aa5
Marcell Pék <marcellpek0@gmail.com>
2018-11-21 19:59:14 +0000

    Change path in iOS certificate add script.

--- signing/ios/add-key.sh
+++ signing/ios/add-key.sh
@@ -14,9 +14,9 @@ security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
-security import ./build-assets/ios/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./build-assets/ios/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./build-assets/ios/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $ENCRYPTION_SECRET -T /usr/bin/codesign
+security import ./signing/ios/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./signing/s/ios/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./signing/ios/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $ENCRYPTION_SECRET -T /usr/bin/codesign
 
 # Fix a bug that stalls code signing process
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
