6a18b018a6443608179b4b5e22b3c48158068589
Marcell Pek <marcellpek0@gmail.com>
2018-12-02 14:52:21 +0000

    Fix issues in build

--- integration_test.bash
+++ integration_test.bash
@@ -1,6 +1,5 @@
-curl -H "Content-type:application/json" -X POST 127.0.01:8080/rest/login --data '{"guid": "admin", "password": "alwyn96327011"}' -c - | awk '{print $7}' | tail -n 1 > token
+curl -H "Content-type:application/json" -X POST 127.0.0.1:8080/rest/login --data '{"guid": "admin", "password": "alwyn96327011"}' -c - | awk '{print $7}' | tail -n 1 > token
 token=$(cat token)
-curl -H "Content-type:application/json" -X POST 127.0.01:8080/rest/getGroups --data '{"guid": "admin"}' --cookie "token=$token" > answer
+curl -H "Content-type:application/json" -X POST 127.0.0.1:8080/rest/getGroups --data '{"guid": "admin"}' --cookie "token=$token" > answer
 cat answer
 if grep -q CS1P answer; then echo "0"; else exit 1; fi
-
