94d1b4261405f6fb0060c1da3e22c8c2f24a9f5f
Marcell Pek <marcellpek0@gmail.com>
2018-12-02 14:44:32 +0000

    Add integration testing and decouple backend build

---
+++ integration_test.bash
@@ -0,0 +1,6 @@
+curl -H "Content-type:application/json" -X POST 127.0.01:8080/rest/login --data '{"guid": "admin", "password": "alwyn96327011"}' -c - | awk '{print $7}' | tail -n 1 > token
+token=$(cat token)
+curl -H "Content-type:application/json" -X POST 127.0.01:8080/rest/getGroups --data '{"guid": "admin"}' --cookie "token=$token" > answer
+cat answer
+if grep -q CS1P answer; then echo "0"; else exit 1; fi
+
